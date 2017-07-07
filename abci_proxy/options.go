a752b111cb87858866b2cde1486762cb9e3bb375
Antoine <antoine@cercle.co>
2017-07-07 11:30:25 +0200

    handle default ABCi app port

--- abci_proxy/options.go
+++ abci_proxy/options.go
@@ -13,10 +13,10 @@ type options struct {
 func ParseOptions() options {
 	var opts options
 
-	flag.StringVar(&opts.Address, "addr", "tcp://0.0.0.0:46658", "Listen address for tendermind node")
+	flag.StringVar(&opts.Address, "addr", "tcp://0.0.0.0:46659", "Listen address for tendermind node")
 	flag.StringVar(&opts.RPCAddress, "rpc", "tcp://0.0.0.0:46660", "listen address for rcp")
 	flag.StringVar(&opts.ABCIType, "abci", "socket", "socket | grpc")
-	flag.StringVar(&opts.AppAddress, "proxy", "tcp://0.0.0.0:46659", "Address of next ABCI app")
+	flag.StringVar(&opts.AppAddress, "proxy", "tcp://0.0.0.0:46658", "Address of next ABCI app")
 	flag.BoolVar(&opts.Verbose, "verbose", false, "verbose output")
 	flag.BoolVar(&opts.Verbose, "v", false, "verbose output")
 	flag.Parse()
